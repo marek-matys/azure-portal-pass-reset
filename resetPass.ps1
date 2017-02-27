a9bcb52aef20ca305d96b8b677da193a50c64743
marek-matys <marek.matys@gmail.com>
2017-02-28 00:33:39 +0100

    Add resetPass.ps1

---
+++ resetPass.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzureRmAccount
+
+$cred=Get-Credential
+
+Set-AzureRmVMAccessExtension -ResourceGroupName "???" -VMName "???" `
+    -Name "???" -Location "West Europe" -UserName $cred.GetNetworkCredential().Username `
+    -Password $cred.GetNetworkCredential().Password -typeHandlerVersion "2.0"
+
+    #https://docs.microsoft.com/pl-pl/azure/virtual-machines/virtual-machines-windows-reset-rdp?toc=%2fazure%2fvirtual-machines%2fwindows%2ftoc.json#vmaccess-extension-and-powershell---resource-manager
